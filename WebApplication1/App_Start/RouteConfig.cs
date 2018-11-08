7d8db8f857a48374ade3fdc230fa9cc9ccb5589d
benzarti <benzimededdine@hotmail.com>
2018-11-08 14:48:54 +0100

    Projet GPAO : Test Recherche

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -13,6 +13,29 @@ namespace WebApplication1
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Custom Route : search
+            routes.MapRoute(
+                "ArticleByReleaseDate",
+                "Article/released/{year}/{month}",
+                new {controller="Article", action="ByReleaseDate"});
+            //Custom Route : search by characters
+
+            routes.MapRoute(
+                "ArticleByReleaseDateChar",
+                "Article/release/{year}/{month}",
+                new { controller = "Article", action = "ByReleaseDatechar" },
+                new { year=@"\d{4}", month=@"\d{2}"});
+
+            //Custom Route : Intervalle entre deux dates Attribute Routing
+
+            routes.MapRoute(
+               "ArticleByReleasePeriodDate",
+               "Article/release/{year}/{month}",
+               new { controller = "Article", action = "ByReleaseDatePeriod" },
+               new { year = @"2017|2018", month = @"\d{2}" });
+
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- WebApplication1/Controllers/ArticleController.cs
+++ WebApplication1/Controllers/ArticleController.cs
@@ -16,6 +16,51 @@ namespace WebApplication1.Controllers
             return View(article);
             //return Content("Hello World");
             //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index","Home", new {page=1, sortBy="name"} );
         }
+
+
+        public ActionResult Edit(int id)
+        {
+
+            return Content("id"+id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex : {0}&sortBy : {1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+"/"+month); 
+        }
+
+        //ByReleaseDatechar
+
+        public ActionResult ByReleaseDatechar(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        //ByReleaseDatePeriod
+
+        public ActionResult ByReleaseDatePeriod(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
     }
 }
