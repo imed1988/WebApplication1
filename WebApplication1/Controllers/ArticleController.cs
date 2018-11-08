4cc0c93cf2f5b7d45996a21cd463811dbce98f7a
benzarti <benzimededdine@hotmail.com>
2018-11-08 12:08:54 +0100

    Projet GPAO : Changement

---
+++ WebApplication1/Controllers/ArticleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Random()
+        {
+            var article = new Article() { DesignationArticle = "SOUFFI GAFSA GI" };
+            return View(article);
+            //return Content("Hello World");
+            //return HttpNotFound();
+        }
+    }
+}
---
+++ WebApplication1/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Article
+    {
+        public string RefArticle { get; set; }
+        public string DesignationArticle { get; set; }
+        public string UniteArticle { get; set; }
+
+    }
+}
